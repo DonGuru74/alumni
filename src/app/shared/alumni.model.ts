5651d3e154f465736d5cebe6f0ef07067a01a440
Misasun1 <misa1996@yahoo.com>
2024-10-21 14:50:14 +0200

    alumni-part1

---
+++ src/app/shared/alumni.model.ts
@@ -0,0 +1,7 @@
+export interface IAlumni{
+    name: string,
+    image: string,
+    age: number,
+    qualification: string,
+    description: string
+}
---
+++ src/app/shared/alumni.ts
@@ -0,0 +1,75 @@
+import { IAlumni } from "./alumni.model";
+
+export const alumni : IAlumni [] = [ 
+    {
+    name:'James Smith',
+    age: 30,
+    image:'/img/man.jpg',
+    qualification: 'Full-Stack Developer' ,
+    description: 'I am thrilled to share my experience with CodeLab, as I recently qualified as a full-stack developer. Throughout the training, I achieved exceptional technical skills and a deep understanding of both front-end and back-end development. I have mastered key technologies such as HTML, CSS, JavaScript, and frameworks like React, showcasing a keen eye for design and user experience. On the back end,I have became proficient in Node.js and Express, effectively building robust APIs and managing databases like MongoDB and PostgreSQL. I am really satisfied with the course and how much dedication had the trainers to enance our skills in a such short period of time!'
+
+},
+
+{
+    name:'Beatrice Rossi',
+    age: 28,
+    image:'/img/sono-bono--LI5_jrU_wM-unsplash.jpg',
+    qualification: 'Data Analyst' ,
+    description: ''
+
+},
+
+{
+    name:'Ama Mensah',
+    age: 23,
+    image:'/img/woman2.jpg',
+    qualification: 'Python Developer' ,
+    description: 'What truly set apart is their problem-solving ability and adaptability. I approached challenges with a positive attitude, often going above and beyond to find innovative solutions. The collaborative spirit made them a valuable team member, and the eagerness to learn new technologies was inspiring.I have no doubt that I will excel in any development role they pursue. Their dedication, creativity, and technical prowess will make them a significant asset to any organization.'
+
+},
+{
+    name:'Sthephen Hops',
+    age: 25,
+    image:'/img/man4.jpg',
+    qualification: 'Front-end Developer' ,
+    description: ''
+
+},
+{
+    name:'Mohamed Al Hamzi',
+    age: 27,
+    image:'/img/man0.jpg',
+    qualification: 'Back-end Developer' ,
+    description: ''
+
+},
+{
+    name:'Samatha Greens',
+    age: 30,
+    image:'/img/woman5.jpg',
+    qualification: 'Jascript Developer' ,
+    description: ''
+
+},
+{
+    name:'Robert Singh',
+    age: 30,
+    image:'/img/man3.jpg',
+    qualification: 'Front-end Developer' ,
+    description: ''
+
+},
+{
+    name:'Priscille Monet',
+    age: 32,
+    image:'/img/woman3.jpg',
+    qualification: 'Full-stack Developer' ,
+    description: ''
+
+}
+
+
+
+
+
+]
