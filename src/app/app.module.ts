3875401687c845041556aced01e9a0d275f8baa1
Misasun1 <misa1996@yahoo.com>
2024-10-22 09:40:20 +0200

    a pp module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,9 @@ import { CardComponent } from './card/card.component';
 import { AluDirectoryComponent } from './alu-directory/alu-directory.component';
 import { CareerComponent } from './career/career.component';
 import { StoriesComponent } from './stories/stories.component';
-<<<<<<< HEAD
 import { FormsModule } from '@angular/forms';
-=======
 import { EventReaderComponent } from './event-reader/event-reader.component';
 import { HomeComponent } from './home/home.component';
->>>>>>> 59fab482c58d5463cca3117fc5e82de0ab34ba7d
 
 @NgModule({
   declarations: [
