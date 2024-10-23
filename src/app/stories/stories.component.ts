c33d34434b05eb0364dc76074de14d71e78ca90a
Mohannad779 <mohanadalatrash7@gmail.com>
2024-10-23 09:19:04 +0200

    0.3

--- src/app/stories/stories.component.ts
+++ src/app/stories/stories.component.ts
@@ -14,17 +14,17 @@ export class StoriesComponent implements OnInit {
     this.stories = [
       { 
         title: 'Ama Mensah',
-        description: 'Alumnus 1 went on to achieve great success in the field of technology, working for leading companies and founding a successful startup.',
+        description: 'Ama went on to achieve great success in the field of technology, working for leading companies and founding a successful startup.',
         image: '/img/woman2.jpg'  
       },
       { 
         title: 'Samantha Greens',
-        description: 'Alumnus 2 became a renowned researcher in the field of biomedical science, contributing to groundbreaking research and innovations.',
+        description: 'Samantha became a renowned researcher in the field of biomedical science, contributing to groundbreaking research and innovations.',
         image: '/img/woman5.jpg'
       },
       { 
         title: 'Priscille Monet',
-        description: 'Alumnus 3 made a significant impact in the non-profit sector, leading global initiatives to promote education and sustainability.',
+        description: 'Priscille made a significant impact in the non-profit sector, leading global initiatives to promote education and sustainability.',
         image: '/img/woman3.jpg'
       }
     ];
